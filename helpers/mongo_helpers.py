4f7518f20068b94961f6ac41ede6927768d30989
Mert <bilgic.mert44@hotmail.com>
2020-03-16 17:05:17 +0300

    mongo_helpers  için singleton pattern uygulandı.

--- helpers/mongo_helpers.py
+++ helpers/mongo_helpers.py
@@ -1,7 +1,22 @@
 import pymongo
 from helpers.config_helpers import get_value_from_name 
 
-libraryClient = pymongo.MongoClient(get_value_from_name('mongodb_client'))
+class MongoSingleton:
+    __instance = None
+
+    @staticmethod 
+    def getInstance():
+        """ Static access method. """
+        if MongoSingleton.__instance == None:
+            MongoSingleton()
+        return MongoSingleton.__instance
+        
+    def __init__(self):
+        """ Virtually private constructor. """
+        if MongoSingleton.__instance == None:
+            MongoSingleton.__instance = pymongo.MongoClient(get_value_from_name('mongodb_client'))
+
+libraryClient = MongoSingleton.getInstance()
 
 library_db = libraryClient[get_value_from_name('db_name')]
 
