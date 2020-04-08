e1770cafe6e4b9f804497ddcb1c1aafd96e8187e
Mert <bilgic.mert44@hotmail.com>
2020-04-09 00:09:42 +0300

    ImageProc sınıfı için alt yapı hazırlandı.

---
+++ helpers/image_proc_helpers.py
@@ -0,0 +1,19 @@
+import pytesseract
+import cv2
+
+class ImageProc:
+    def __init__(self, config):
+        self.image_path = None
+        self.result = dict()
+
+    @staticmethod
+    def proc_image(self):
+        pass
+
+    @staticmethod
+    def read_ISBN(self):
+        pass
+
+    @staticmethod
+    def get_result(self):
+        pass
